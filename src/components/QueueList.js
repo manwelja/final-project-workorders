019c4e691246b7d8869f9fbbea0f802ec4e6dee9
Jennifer Manwell <manwell.fortier@gmail.com>
2022-06-10 19:13:32 -0400

    Add props to WorkOrderList component

--- src/components/QueueList.js
+++ src/components/QueueList.js
@@ -1,1 +1,24 @@
-// JEN
+import React from "react";
+import QueueListItem from "components/QueueListItem";
+
+//Component that invokes a QueueListItem child for each workorder in the data set
+export default function QueueList(props) {
+  const queueItem = props.workorders.map((workOrderData) => {
+    //return a populated Queuelist item for each workorder in the data set
+    return(
+      <QueueListItem
+          key={ workOrderData.id }
+          environment={ workOrderData.environment } 
+          description={ workOrderData.description } 
+          dateCreated={ workOrderData.date_created }
+          studentFirstName= { workOrderData.student_first_name }
+          studentLastName= { workOrderData.student_last_name }
+          topic= { workOrderData.topic }
+        />) 
+    });
+
+    return(
+      <ul>{ queueItem }</ul>
+    )
+
+};
--- src/components/QueueListItem.js
+++ src/components/QueueListItem.js
@@ -3,16 +3,23 @@ import classNames from "classnames";
 
 //Component that displays individual interviewer data
 export default function QueueListItem(props) {
-  const workorder = props.workorder;
+  console.log(props)
+  const { dateCreated, studentFirstName, studentLastName, environment, description }  = props;
   //return an item for each workorder passed in as a prop
-    return (
+  const options = { year: "numeric", month: "long", day: "numeric" }
+  const formattedDate =  new Date(dateCreated).toLocaleDateString(undefined, options)
+  const formattedTime = new Date(dateCreated).toLocaleTimeString('en-US')
+  const formattedDateTime = formattedDate +  " - " + formattedTime;
+  
+ 
+   return (
       <div class="card">
         <img class="card-img-top" src="" alt="Student avatar and name" />
         <div class="card-body">
-          <h5 class="card-title">Created { workorder.date_created }</h5>
-          <p class="card-text">Student Name: { workorder.first_name } + " " + { workorder.last_name }</p>
-          <p class="card-text">Environment: { workorder.environment }</p>
-          <p class="card-text">Description: { workorder.description }</p>
+          <h5 class="card-title">Created { formattedDateTime }</h5>
+          <p class="card-text">Student Name: { studentFirstName + " " +  studentLastName }</p>
+          <p class="card-text">Environment: { environment }</p>
+          <p class="card-text">Description: { description }</p>
           <a href="#" class="btn btn-primary">See user History</a>
           <a href="#" class="btn btn-primary">View</a>
         </div>
