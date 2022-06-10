e68cd3ba2d97ede8c2dcac136cea47af79555d5c
Aaron Au <aaron.au@me.com>
2022-06-10 16:00:29 -0600

    Initial commit. Set up form component skeleton

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,3 +1,17 @@
 // COMPONENT THAT PULLS WORKORDER INFORMATION AND DISPLAYS IT
 // AARON
 // workorder_from_queue -> what mentor sees when they open the link
+import React from "react";
+import NewWorkorder from "./Form/Workorder_new";
+
+const Form = (props) => {
+  return (
+    <article>
+      <h1>Form Component</h1>
+      {/* will need to pass in props here */}
+      <NewWorkorder />
+    </article>
+  );
+};
+
+export default Form; 
--- src/components/Form/Workorder_new.js
+++ src/components/Form/Workorder_new.js
@@ -1,1 +1,12 @@
 // form when student is creating a new workorder
+import React from "react";
+
+const NewWorkorder = (props) => {
+  return (
+    <section>
+      <h2>New Workorder Component</h2>
+    </section>
+  );
+};
+
+export default NewWorkorder;
