1caec444242b6d2edb4ecab81f2007566ca9899b
Jennifer Manwell <manwell.fortier@gmail.com>
2022-06-12 18:48:18 -0400

    Add save button and persist base data to database through API

---
+++ src/components/Button.js
@@ -0,0 +1,20 @@
+import React from "react";
+import classNames from "classnames"
+
+export default function Button(props) {
+   
+   const buttonClass = classNames("button", {
+      "button--confirm": props.confirm,
+      "button--danger": props.danger
+   }); 
+
+   return (
+     <button 
+       className={buttonClass} 
+       onClick={props.onClick}>
+          {props.children}
+     </button>
+   );
+}
+
+
--- src/components/Form/NewWorkorder.js
+++ src/components/Form/NewWorkorder.js
@@ -1,6 +1,7 @@
 // form when student is creating a new workorder
 import React, { useState, useEffect, Fragment } from "react";
 import axios from 'axios';
+import Button from "../Button"
 
 export default function NewWorkorder(props){
  
@@ -8,14 +9,16 @@ export default function NewWorkorder(props){
   const [selectedModule, setSelectedModule] = React.useState({selectedModule: ""});
   const [category, setCategory] = useState([{}]);
   const [selectedCategory, setSelectedCategory] = React.useState({selectedCategory: ""});
+  const PORT = process.env.API_PORT;
+  const HOST = process.env.API_HOST;
+  const BASE_URL = HOST + ":" + PORT;
+  console.log(BASE_URL)
 
   //populate the schedule when the application loads
   useEffect(() => {        
-    Promise.all([      
-      
-      //console.log("port", process.env.API_PORT);
-      axios.get("http://localhost:8001/api/modules"),
-      axios.get("http://localhost:8001/api/categories"),
+    Promise.all([                       
+      axios.get(`http://${BASE_URL}/api/modules`),
+      axios.get(`http://${BASE_URL}/api/categories`),
       //axios.get("/api/interviewers")
     ]).then((all) => {    
         //const modules = all[0].data.map((entry, idx) => [{label: entry.id, value: entry.topic}].flatten)
@@ -37,7 +40,16 @@ export default function NewWorkorder(props){
     console.log(selectedCategory)
   };
 
-  useEffect(() => {        
+  const validate = () => {
+    console.log('here')
+     return axios.put(`http://localhost:8001/api/workorders`, {user_student_id: 1, status_id: 1, category_id: selectedCategory, module_id: selectedModule, environment: "M1", Description: "Test"})
+       .then((res) => {
+        console.log(res)
+         return;
+       })
+     }
+
+     useEffect(() => {        
     console.log("selectedModule", selectedModule)    
   }
   ,[selectedModule])
@@ -45,88 +57,87 @@ export default function NewWorkorder(props){
   return (
     <main>
       <article>
-        <section>
-          <h1>New Help Request</h1>
-          <form>
-            <section>
-              <label>Link to instructions</label>
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+          <section>
+            <h1>New Help Request</h1>
+            <form>
+              <section>
+                <label>Link to instructions</label>
+                <br />
+                <input
+                  name="link"
+                  type="text"
+                  placeholder="Enter URL here"
+                  style={{ width: "500px" }}
+                />
+              </section>
+
               <br />
-              <input
-                name="link"
-                type="text"
-                placeholder="Enter URL here"
-                style={{ width: "500px" }}
-              />
+
+              <section>
+                <label>Please describe your issue</label>
+                <br />
+                <input
+                  name="description"
+                  type="text"
+                  placeholder="Tell us what's up"
+                  style={{ width: "500px", height: "200px" }}
+                />
+              </section>
+            </form>
+          </section>
+
+          <section>
+            <section>
+              <p>Please specify your computer environment</p>
+              <p></p>
+              <button>M1</button>
+              <button>Vagrant (Mac)</button>
+              <button>Windows</button>
             </section>
 
             <br />
 
             <section>
-              <label>Please describe your issue</label>
-              <br />
-              <input
-                name="description"
-                type="text"
-                placeholder="Tell us what's up"
-                style={{ width: "500px", height: "200px" }}
-              />
+              <p>Please specify the category</p>
+              <select value={selectedCategory} onChange={handleCategoryChange}>
+                {category.map((option) => (
+                <option value={option.id}>{option.description}</option>
+                ))}
+              </select> 
             </section>
-          </form>
-        </section>
 
-        <section>
-          <section>
-            <p>Please specify your computer environment</p>
-            <p></p>
-            <button>M1</button>
-            <button>Vagrant (Mac)</button>
-            <button>Windows</button>
+            <section>
+              <p>Please specify which module you're working on:</p>  
+              <select value={selectedModule} onChange={handleModuleChange}>
+                {module.map((option) => (
+                <option value={option.id}>{option.topic}</option>
+                ))}
+              </select> 
+            </section>
+
+            <br />
+            <br />
+
+            <section>
+              <form>
+                <label>Optional: Upload screenshot(s)</label>
+                <br />
+                <input
+                  name="screenshot"
+                  type="file"
+                  multiple
+                />
+                <br />
+                <br />
+              </form>
+            </section>
           </section>
-
           <br />
-
-          <section>
-            <p>Please specify the category</p>
-            <select value={selectedCategory} onChange={handleCategoryChange}>
-              {category.map((option) => (
-              <option value={option.id}>{option.description}</option>
-              ))}
-            </select> 
-          </section>
-
-          <section>
-            <p>Please specify which module you're working on:</p>  
-            <select value={selectedModule} onChange={handleModuleChange}>
-              {module.map((option) => (
-              <option value={option.id}>{option.topic}</option>
-              ))}
-            </select> 
-          </section>
-
-          <br />
-          <br />
-
-          <section>
-            <form>
-              <label>Optional: Upload screenshot(s)</label>
-              <br />
-              <input
-                name="screenshot"
-                type="file"
-                multiple
-              />
-              <br />
-              <br />
-
-              <button>Submit</button>
-            </form>
-          </section>
-        </section>
-        <br />
-
+        </form>
         <section>
           <button>Cancel</button>
-          <button>Submit Request</button>
+          <Button confirm onClick={ () => { validate() }}>Save Me</Button>
         </section>
       </article>
     </main>
