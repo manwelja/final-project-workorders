751f7e345894b701f001ca43b0113bff08cc1aad
Aaron Au <aaron.au@me.com>
2022-06-13 15:12:52 -0600

    Implementing put request to add feedback to mentor

--- src/components/Form/StudentFeedbackForm.js
+++ src/components/Form/StudentFeedbackForm.js
@@ -3,6 +3,35 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const StudentFeedbackForm = (props) => {
+  const BASE_URL = 'http://localhost:8001';
+  const [rating, setRating] = useState(0);
+  const [workorder, setWorkorder] = useState([{}]);
+  const [notes, setNotes] = useState("");
+
+  useEffect(() => {
+    Promise.all([
+      axios.get(`${BASE_URL}/api/workorders/1`),
+      axios.get(`${BASE_URL}/api/users`)
+    ])
+      .then((all) => {
+        console.log('in get req.');
+        console.log(all[0].data[0]);
+        setWorkorder(all[0].data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
+  const saveData = () => {
+    console.log(`save data call:`);
+    console.log(workorder);
+    setWorkorder([{ ...workorder, "student_notes": notes }]);
+
+    //use put using update in route .../api/update/workorders/1
+    //sql: 
+    // axios.patch(`${BASE_URL}/api/workorders/1`,);
+  };
 
   return (
     <article>
@@ -23,16 +52,18 @@ const StudentFeedbackForm = (props) => {
       </section>
 
       <section>
-        <form>
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <label>Please Provide Feedback For Your Interaction:</label>
           <br />
           <input
             type="text"
             name="student_notes"
             placeholder="Tell us how we did"
+            value={notes}
+            onChange={event => { setNotes(event.target.value); }}
           />
           <br />
-          <button>Submit Feedback</button>
+          <button onClick={() => saveData()}>Submit Feedback</button>
         </form>
 
       </section>
