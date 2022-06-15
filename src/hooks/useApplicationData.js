b368e8cfe1b4c6cc7e9c4c9058e3668bf6ee93ff
Jennifer Manwell <manwell.fortier@gmail.com>
2022-06-15 16:02:12 -0400

    Add useApplicationData

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -4,6 +4,20 @@ import axios from "axios";
 //function responsible for managing all state changes in the application 
 export default function useApplicationData() {
 
+  const getQueueListByStatus = function (workorderStatus) {
+
+  }
+
+  const getQueueListByMentor = function (mentorID) {
+
+  }
+
+  const getWorkorderListByStudent = function(studentID) {
+
+  }
   
-  return { };
+  const getWorkorderByID = function(workorderID) {
+
+  }
+  return { getQueueListByStatus, getQueueListByMentor, getWorkorderListByStudent, getWorkorderByID };
 }
