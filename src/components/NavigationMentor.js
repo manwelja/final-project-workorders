9370e1f863417a2e7e12d45d631a3022f8f0a02d
Emma Grannis <grannis.emma@gmail.com>
2022-06-19 19:07:25 -0400

    Fix nav bar to show logged in as: above the buttons

--- src/components/NavigationMentor.js
+++ src/components/NavigationMentor.js
@@ -9,22 +9,24 @@ export default function NavigationStudent(props) {
   return (
     <div class="nav-container">
       <div class="nav-app-logo"><img class="nav-app-logo" src={logoUrl} alt="Logo" /></div>
-      <div class="nav-container-buttons">
+      <div class="nav-container-buttons-email">
         <div class="nav-login-email">Logged in as: {props.email}</div>
-        <div class="nav-container-button">
-          <div><Button className="button--top-nav" top-nav onClick={onShowNew}>Workorders Queue</Button></div>
-        </div>
-        <div class="nav-container-button">
-          <div><Button className="button--top-nav" top-nav onClick={onShowInProgress}>In Progress Workorders</Button></div>
-        </div>
-        <div class="nav-container-button">
-          <div><Button className="button--top-nav" top-nav onClick={onShowClosed}>Closed Workorders</Button></div>
-        </div>
-        <div class="nav-container-button">
-          <div><Button className="button--top-nav" top-nav onClick={onShowMy}>My Workorders</Button></div>
-        </div>
-        <div class="nav-container-button">
-          <div><Button className="button--top-nav" top-nav onClick={onLogout}>Logout</Button></div>
+        <div class="nav-container-buttons">
+          <div class="nav-container-button">
+            <div><Button className="button--top-nav" top-nav onClick={onShowNew}>Workorders Queue</Button></div>
+          </div>
+          <div class="nav-container-button">
+            <div><Button className="button--top-nav" top-nav onClick={onShowInProgress}>In Progress Workorders</Button></div>
+          </div>
+          <div class="nav-container-button">
+            <div><Button className="button--top-nav" top-nav onClick={onShowClosed}>Closed Workorders</Button></div>
+          </div>
+          <div class="nav-container-button">
+            <div><Button className="button--top-nav" top-nav onClick={onShowMy}>My Workorders</Button></div>
+          </div>
+          <div class="nav-container-button">
+            <div><Button className="button--top-nav" top-nav onClick={onLogout}>Logout</Button></div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/NavigationStudent.js
+++ src/components/NavigationStudent.js
@@ -7,16 +7,18 @@ export default function NavigationStudent(props) {
   return (
     <div class="nav-container">
       <div class="nav-app-logo"><img class="nav-app-logo" src={logoUrl} alt="Logo" /></div>
-      <div class="nav-container-buttons">
+      <div class="nav-container-buttons-email">
         <div class="nav-login-email">Logged in as: {props.email}</div>
-        <div class="nav-container-button">
-          <div><Button className="button--top-nav" top-nav onClick={onView}>My Workorders</Button></div>
-        </div>
-        <div class="nav-container-button">
-          <div><Button className="button--top-nav" top-nav onClick={onNew}>New Workorder</Button></div>
-        </div>
-        <div class="nav-container-button">
-          <div><Button className="button--top-nav" top-nav onClick={onLogout}>Logout</Button></div>
+        <div class="nav-container-buttons">
+          <div class="nav-container-button">
+            <div><Button className="button--top-nav" top-nav onClick={onView}>My Workorders</Button></div>
+          </div>
+          <div class="nav-container-button">
+            <div><Button className="button--top-nav" top-nav onClick={onNew}>New Workorder</Button></div>
+          </div>
+          <div class="nav-container-button">
+            <div><Button className="button--top-nav" top-nav onClick={onLogout}>Logout</Button></div>
+          </div>
         </div>
       </div>
     </div>
