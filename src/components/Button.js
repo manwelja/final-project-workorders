53c8f00013497079ec3ef4aa74213734311e7362
Aaron Au <aaron.au@me.com>
2022-06-19 18:53:56 -0600

    Implement conditional styling for nav bar buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,20 +1,25 @@
 import React from "react";
-import classNames from "classnames"
-import "./Button.css"
+import classNames from "classnames";
+import "./Button.css";
 
 export default function Button(props) {
-   
+
    const buttonClass = classNames("button", {
       "button--confirm": props.confirm,
-      "button--danger": props.danger
-   }); 
+      "button--danger": props.danger,
+      "highlight--queue": props.queue === "SHOW_QUEUE",
+      "highlight--progress": props.progress === "SHOW_IN_PROG",
+      "highlight--closed": props.closed === "SHOW_CLOSED",
+      "highlight--wo": props.workorders === "SHOW_MY_WO"
+   });
+   console.log('buttonClass', buttonClass);
 
    return (
-     <button 
-       className={buttonClass} 
-       onClick={props.onClick}>
-          {props.children}
-     </button>
+      <button
+         className={buttonClass}
+         onClick={props.onClick}>
+         {props.children}
+      </button>
    );
 }
 
--- src/components/NavigationMentor.js
+++ src/components/NavigationMentor.js
@@ -13,16 +13,16 @@ export default function NavigationStudent(props) {
         <div class="nav-login-email">Logged in as: {props.email}</div>
         <div class="nav-container-buttons">
           <div class="nav-container-button">
-            <div><Button className="button--top-nav" top-nav onClick={onShowNew}>Workorders Queue</Button></div>
+            <div><Button className="button--top-nav" top-nav onClick={onShowNew} queue={mode}>Workorders Queue</Button></div>
           </div>
           <div class="nav-container-button">
-            <div><Button className="button--top-nav" top-nav onClick={onShowInProgress}>In Progress Workorders</Button></div>
+            <div><Button className="button--top-nav" top-nav onClick={onShowInProgress} progress={mode}>In Progress Workorders</Button></div>
           </div>
           <div class="nav-container-button">
-            <div><Button className="button--top-nav" top-nav onClick={onShowClosed}>Closed Workorders</Button></div>
+            <div><Button className="button--top-nav" top-nav onClick={onShowClosed} closed={mode}>Closed Workorders</Button></div>
           </div>
           <div class="nav-container-button">
-            <div><Button className="button--top-nav" top-nav onClick={onShowMy}>My Workorders</Button></div>
+            <div><Button className="button--top-nav" top-nav onClick={onShowMy} workorders={mode}>My Workorders</Button></div>
           </div>
           <div class="nav-container-button">
             <div><Button className="button--top-nav" top-nav onClick={onLogout}>Logout</Button></div>
