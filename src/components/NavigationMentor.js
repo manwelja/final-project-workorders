7402a0cece6cd421a9f2ce0c00bcda7f40ad9218
Jennifer Manwell <manwell.fortier@gmail.com>
2022-06-21 18:34:32 -0400

    Clean up dropdown button style

--- src/components/NavigationMentor.js
+++ src/components/NavigationMentor.js
@@ -4,7 +4,7 @@ import Button from "./Button";
 import Dropdown from "react-bootstrap/Dropdown";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
-
+import classNames from "classnames";
 // Component that renders Navigation bar with proper buttons displayed for mentor view
 
 export default function NavigationMentor(props) {
@@ -17,6 +17,15 @@ export default function NavigationMentor(props) {
     setMenuValue(e);
   };
 
+  
+
+  const dropdownHeaderClass = classNames("queue-dropdown-menu", {
+    "--white-background": mode === "SHOW_QUEUE" || mode === "SHOW_IN_PROG" || mode === "SHOW_CLOSED",
+  });
+  const dropdownDivClass = classNames("nav-container-button-dropdown", {
+    "--white-background": mode === "SHOW_QUEUE" || mode === "SHOW_IN_PROG" || mode === "SHOW_CLOSED",
+  });
+  
   return (
     <div class="nav-container">
       <div class="nav-app-logo"><img class="nav-app-logo" src={logoUrl} alt="Logo" /></div>
@@ -25,54 +34,40 @@ export default function NavigationMentor(props) {
         <div class="nav-container-buttons">
 
           <style type="text/css">
-            {`
-        .queue-dropdown-menu {
-          background: transparent;
-          color: white;
-          font-size: 1.25rem;
-          width: 10rem;
-          height: 4rem;   
-          border: transparent;       
-        }
-        .queue-dropdown-menu-item {
-          background: transparent;
-          color: white;
-          font-size: 1.1rem;   
-          border: transparent;       
-        }
-        .queue-dropdown-menu-item:focus {
-          background: white;
-          color: #657860;
-          border: transparent;
-        }
-        .header{
-          color: white;
-          border: none;
-          border: solid 1px white;
-        }      
-        .header:hover {
-          color: #657860;  
-          border: solid 1px white;
-        }
+            {`  
+            .queue-dropdown-menu {
+                background: transparent;
+                color: white;
+                font-size: 1.25rem;
+                width: 10rem;
+                height: 4rem;   
+                border: none;       
+              }
+              .queue-dropdown-menu:hover {
+                color: #657860;                   
+              }
+              .queue-dropdown-menu-item {
+                background: transparent;
+                color: #657860;
+                font-size: 1.1rem;   
+                border: transparent;       
+              }
+              .queue-dropdown-menu-item:hover {
+                font-weight: bold;
+                color: #657860;                    
+              }
+              .--white-background {
+                background: white; 
+                color: #657860;  
+              }
 
-        .queue-dropdown-menu:hover {
-          background: transparent;
-          color: #657860;
-          border: transparent;
-        }        
-        .queue-dropdown-menu:focus {
-          background: white;
-          color: #657860;
-          border: transparent;
-        }
-
-      `}
+            `}
           </style>
 
-          <div class="nav-container-button-dropdown">
-            <Dropdown as={ButtonGroup} onSelect={handleSelect} className="queue-dropdown-menu header">
-              <span><Dropdown.Toggle className="queue-dropdown-menu header" variant="outline-light"> {menuValue}</Dropdown.Toggle></span>
-              <Dropdown.Menu className="queue-dropdown-menu" >
+          <div class={dropdownDivClass}>
+            <Dropdown as={ButtonGroup} onSelect={handleSelect}>
+              <span><Dropdown.Toggle className={dropdownHeaderClass} variant="outline-light"> {menuValue}</Dropdown.Toggle></span>
+              <Dropdown.Menu>
                 <Dropdown.Item className="queue-dropdown-menu-item" onClick={onShowNew} eventKey="Queue">Queue</Dropdown.Item>
                 <Dropdown.Item className="queue-dropdown-menu-item" onClick={onShowInProgress} eventKey="In Progress">In Progress</Dropdown.Item>
                 <Dropdown.Item className="queue-dropdown-menu-item" onClick={onShowClosed} eventKey="Completed">Completed</Dropdown.Item>
@@ -90,4 +85,3 @@ export default function NavigationMentor(props) {
     </div>
   );
 };
-//className="nav-dropdown"
